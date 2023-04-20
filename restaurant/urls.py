62bb82f815f42a583a1d05a6572184432e8078d0
K Butt <kam_butt@hotmail.com>
2023-04-20 15:30:33 -0400

    0420

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-urlpatterns = [
-    path('',views.index,name='index'),    
+#For APIView Viewset based views
+from rest_framework.routers import DefaultRouter
+#Create router
+router = DefaultRouter()
+#Register views based on viewsets with router
+router.register(r'menuv',views.MenuViewset,basename='menuv')
+router.register(r'bookingv',views.BookingViewset,basename='bookingv')
+#Register views based on Modelviewsets with router
+router.register(r'menum',views.MenuModelViewset,basename='menum')
+router.register(r'bookingm',views.BookingModelViewset,basename='bookingm')
+
+urlpatterns = [    
+    path('menu',views.MenuView.as_view()),
+    path('booking',views.BookingView.as_view()),
+    #paths for viewsets registered with router    
+    path('',include(router.urls)),
+    #paths for Generic views
+    path('menug',views.MenuGenericView.as_view()),
+    path('bookingg',views.BookingGenericView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,9 +1,200 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def sayHello(request):    
-    return HttpResponse('Hello World!' + str(request))
+#For APIs
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-def index(request):
-    return render(request, 'index.html',{})
+#For APIs using viewsets
+from rest_framework import viewsets
+from rest_framework import status
+
+#For APIs using Generic views
+from rest_framework import generics
+
+#For authentication
+from rest_framework.permissions import IsAuthenticated
+# Create your views here.
+#API views for models based on their serializers
+
+class MenuView(APIView):
+    
+    def get(self,request):
+        #requires a queryset
+        queryset = Menu.objects.all()
+        #requires a serializer to convert database data into JSON data
+        serializer = MenuSerializer(queryset, many = True)
+        return Response(serializer.data)
+    
+    def post (self, request):
+        #does not require a queryset
+        #POST request has data to be added to database.  First convert it into JSON using serializer made for the model
+        serializer = MenuSerializer(data=request.data)
+        #Check if the submitted data is valid as per model requirement
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":"success", "data":serializer.data})
+        
+
+class BookingView(APIView):
+    def get(self,request):
+        queryset = Booking.objects.all()
+        serializer = BookingSerializer(queryset, many = True)
+        return Response(serializer.data)
+    
+    def post (self, request):
+        #POST request has data to be added to database.  First convert it into JSON using serializer made for the model
+        serializer = BookingSerializer(data=request.data)
+        #Check if the submitted data is valid as per model requirement
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":"success", "data":serializer.data})
+#-----------------------------------------------------------------------------------------------------------------
+#API views using viewsets
+
+class MenuViewset(viewsets.ViewSet):
+    #Require a queryset
+    queryset = Menu.objects.all()
+    #Require a serializer to convert the queryset into JSON
+    serializer_class = MenuSerializer
+    #Will apply to all methods
+    Permission_classes = [IsAuthenticated]    
+    #list instead of get
+    def list(self, request):
+        #will apply to only this method
+        Permission_classes = [IsAuthenticated]
+        #Require a queryset
+        queryset = self.queryset        
+        #Require a serializer to convert the queryset into JSON
+        serializer = self.serializer_class(queryset,many=True)
+        return Response(serializer.data)
+    
+    #create instead of post
+    def create(self, request):
+        #does not require a queryset        
+        #Require a serializer to convert the request data into JSON
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status': 'Data Created'}, status=status.HTTP_201_CREATED)
+
+class BookingViewset(viewsets.ViewSet):
+    #Require a queryset
+    queryset = Booking.objects.all()
+    #Require a serializer to convert the queryset into JSON
+    serializer_class = BookingSerializer
+        
+    #list instead of get
+    def list(self, request):
+        #Require a queryset
+        queryset = self.queryset        
+        #Require a serializer to convert the queryset into JSON
+        serializer = self.serializer_class(queryset,many=True)
+        return Response(serializer.data)
+    
+    #create instead of post
+    def create(self, request):
+        #does not require a queryset        
+        #Require a serializer to convert the request data into JSON
+        serializer = self.serializer_class(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status': 'Data Created'}, status=status.HTTP_201_CREATED)
+#-------------------------------------------------------------------------------------------------------------------------
+#API views using modelviewsets
+
+class MenuModelViewset(viewsets.ModelViewSet):
+    #Require a queryset
+    queryset = Menu.objects.all()
+    #Require a serializer to convert the queryset into JSON
+    serializer_class = MenuSerializer
+        
+    #list instead of get
+    def list(self, request):
+        #Require a queryset
+        queryset = self.queryset        
+        #Require a serializer to convert the queryset into JSON
+        serializer = self.serializer_class(queryset,many=True)
+        return Response(serializer.data)
+    
+    #create instead of post
+    def create(self, request):
+        #does not require a queryset        
+        #Require a serializer to convert the request data into JSON
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status': 'Data Created'}, status=status.HTTP_201_CREATED)
+
+class BookingModelViewset(viewsets.ModelViewSet):
+    #Require a queryset
+    queryset = Booking.objects.all()
+    #Require a serializer to convert the queryset into JSON
+    serializer_class = BookingSerializer
+        
+    #list instead of get
+    def list(self, request):
+        #Require a queryset
+        queryset = self.queryset        
+        #Require a serializer to convert the queryset into JSON
+        serializer = self.serializer_class(queryset,many=True)
+        return Response(serializer.data)
+    
+    #create instead of post
+    def create(self, request):
+        #does not require a queryset        
+        #Require a serializer to convert the request data into JSON
+        serializer = self.serializer_class(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status': 'Data Created'}, status=status.HTTP_201_CREATED)
+#--------------------------------------------------------------------------------------------------
+#API Views using Generic views        
+
+class MenuGenericView(generics.ListCreateAPIView):
+    #Require a queryset
+    queryset = Menu.objects.all()
+    #Require a serializer to convert the queryset into JSON
+    serializer_class = MenuSerializer
+        
+    #list instead of get
+    def list(self, request):
+        #Require a queryset
+        queryset = self.get_queryset()       
+        #Require a serializer to convert the queryset into JSON
+        serializer = self.serializer_class(queryset,many=True)
+        return Response(serializer.data)
+    
+    #create instead of post
+    def create(self, request):
+        #does not require a queryset        
+        #Require a serializer to convert the request data into JSON
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status': 'Data Created'}, status=status.HTTP_201_CREATED)
+
+class BookingGenericView(generics.ListCreateAPIView):
+    #Require a queryset
+    queryset = Booking.objects.all()
+    #Require a serializer to convert the queryset into JSON
+    serializer_class = BookingSerializer
+        
+    #list instead of get
+    def list(self, request):
+        #Require a queryset
+        queryset = self.get_queryset()
+        #Require a serializer to convert the queryset into JSON
+        serializer = self.serializer_class(queryset,many=True)
+        return Response(serializer.data)
+    
+    #create instead of post
+    def create(self, request):
+        #does not require a queryset        
+        #Require a serializer to convert the request data into JSON
+        serializer = self.serializer_class(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status': 'Data Created'}, status=status.HTTP_201_CREATED)
